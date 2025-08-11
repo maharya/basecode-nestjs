7c233d269914fab17dd9f3efed3d5ab25d3aea0f
Arya Mahardika <arya@rollingglory.com>
2025-08-11 22:22:57 +0700

    feat(lint): update typescript-eslint require-await rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,8 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-enum-comparison": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-return": "off"
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/require-await": "off"
     },
   },
 );
